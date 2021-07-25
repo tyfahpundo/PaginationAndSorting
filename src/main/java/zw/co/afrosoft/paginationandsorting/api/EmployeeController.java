65869a3e8df9ab6cd082db18006ac3b3892f11df
tyfah <tafadzwaallentyfah@gmail.com>
2021-07-25 07:16:10 +0200

    removed the unnecessary clater

--- src/main/java/zw/co/afrosoft/paginationandsorting/api/EmployeeController.java
+++ src/main/java/zw/co/afrosoft/paginationandsorting/api/EmployeeController.java
@@ -1,19 +1,14 @@
 package zw.co.afrosoft.paginationandsorting.api;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import zw.co.afrosoft.paginationandsorting.domain.Employee;
 import zw.co.afrosoft.paginationandsorting.service.EmployeeService;
-import zw.co.afrosoft.paginationandsorting.service.EmployeeServiceImpl;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
--- src/main/java/zw/co/afrosoft/paginationandsorting/service/EmployeeServiceImpl.java
+++ src/main/java/zw/co/afrosoft/paginationandsorting/service/EmployeeServiceImpl.java
@@ -2,14 +2,10 @@ package zw.co.afrosoft.paginationandsorting.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import zw.co.afrosoft.paginationandsorting.domain.Employee;
 import zw.co.afrosoft.paginationandsorting.persistence.EmployeeRepository;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class EmployeeServiceImpl implements EmployeeService{
     @Autowired
